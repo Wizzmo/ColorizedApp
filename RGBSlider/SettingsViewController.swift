c0ee04ee4648dd8d8dad524659329a6e854477c8
Maxim Makarov <makarovmaxqa@gmail.com>
2023-12-10 22:09:15 +0100

    Add logic for settings view controller, changesettings in text fields

--- RGBSlider/SettingsViewController.swift
+++ RGBSlider/SettingsViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 final class SettingsViewController: UIViewController {
 
     // MARK: - IB Outlets
-    @IBOutlet var colourPanelView: UIView!
+    @IBOutlet var colorPanelView: UIView!
     
-    @IBOutlet var redColourIntensityLabel: UILabel!
-    @IBOutlet var greenColourIntensityLabel: UILabel!
-    @IBOutlet var blueColourIntensityLabel: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
@@ -32,42 +32,123 @@ final class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colourPanelView.layer.cornerRadius = 20
+        colorPanelView.layer.cornerRadius = 20
         
-        changeViewBackgroundColour()
-        setupLabels()
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        
+        colorPanelView.backgroundColor = viewColor
+        
+        setValue(for: redSlider, greenSlider, blueSlider)
+        setValue(for: redLabel, greenLabel, blueLabel)
+        setValue(for: redTextField, greenTextField, blueTextField)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     // MARK: - IB Actions
-    @IBAction func redSliderValueChanged() {
-        changeViewBackgroundColour()
-        redColourIntensityLabel.text = String(format: "%.2f", redSlider.value)
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            setValue(for: redLabel)
+            setValue(for: redTextField)
+        case greenSlider:
+            setValue(for: greenLabel)
+            setValue(for: greenTextField)
+        default:
+            setValue(for: blueLabel)
+            setValue(for: blueTextField)
+        }
+        
+        setColor()
     }
     
-    @IBAction func greenSliderValueChanged() {
-        changeViewBackgroundColour()
-        greenColourIntensityLabel.text = String(format: "%.2f", greenSlider.value)
-    }
-    
-    @IBAction func blueSliderValueChanged() {
-        changeViewBackgroundColour()
-        blueColourIntensityLabel.text = String(format: "%.2f", blueSlider.value)
+    @IBAction func doneButtonPressed() {
+        delegate.setColor(colorPanelView.backgroundColor ?? .white)
+        dismiss(animated: true)
     }
     
     // MARK: - Private Methods
     private func setupLabels() {
-        redColourIntensityLabel.text = String(round(redSlider.value  * 100) / 100)
-        greenColourIntensityLabel.text = String(round(greenSlider.value  * 100) / 100)
-        blueColourIntensityLabel.text = String(round(blueSlider.value  * 100) / 100)
+        redLabel.text = String(round(redSlider.value  * 100) / 100)
+        greenLabel.text = String(round(greenSlider.value  * 100) / 100)
+        blueLabel.text = String(round(blueSlider.value  * 100) / 100)
     }
     
     private func changeViewBackgroundColour() {
-        colourPanelView.backgroundColor = UIColor(
+        colorPanelView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1.0
         )
+    }
+}
+
+// MARK: - Private Methods
+extension SettingsViewController {
+    private func setColor() {
+        colorPanelView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel: label.text = string(from: redSlider)
+            case greenLabel: label.text = string(from: greenSlider)
+            default: label.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTextField: textField.text = string(from: redSlider)
+            case greenTextField: textField.text = string(from: greenSlider)
+            default: textField.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setValue(for colorSloders: UISlider...) {
+        let ciColor = CIColor(color: viewColor)
+        colorSloders.forEach { slider in
+            switch slider {
+            case redSlider: redSlider.value = Float(ciColor.red)
+            case greenSlider: greenSlider.value = Float(ciColor.green)
+            default: blueSlider.value = Float(ciColor.blue)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            textField?.text = "0.50"
+            textField?.becomeFirstResponder()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
     }
 }
 
